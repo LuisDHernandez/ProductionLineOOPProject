bd2a6891e3544a60a12131fa0626aeda2725ffdb
cobol <cobolongo22@gmail.com>
2019-09-30 12:33:10 -0400

    Added comboBox code and fxml for buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,63 @@
 package sample;
 
-/** @author Luis Hernandez */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+/** @author Luis Hernandez 9/20/2019 */
 public class Controller {
   /** */
+  private Connection conn;
+
+  static final String JDBC_DRIVER = "org.h2.Driver";
+  static final String DB_URL = "jdbc:h2:./res/ProdLineDB";
+
+  @FXML private ComboBox<Integer> cbxQuantity;
+
+  @FXML
+  void btnAddProduct(ActionEvent event) {
+    try (Statement statement = conn.createStatement()) {
+      statement.execute(
+          "INSERT INTO Product(type, manufacturer, name) VALUES"
+              + " ( 'AUDIO', 'Apple', 'iPod' );");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @FXML
+  void btnRecordProduction(ActionEvent event) {
+
+    System.out.println("Product was recorded");
+  }
+
+  void Initialize() {
+
+    //  Database credential
+    conn = null;
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      // STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL);
+
+      // Clean-up environment
+      conn.close();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    cbxQuantity.getItems().addAll(1,2,3,4,5,6,7,8,9,10);
+    cbxQuantity.setEditable(true);
+    cbxQuantity.getSelectionModel().selectFirst();
+  }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 /**
  * @author Luis Hernandez
+ * 9/30/2019
  */
 public class Main extends Application {
 
@@ -22,8 +23,8 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("Hello World");
-    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.setTitle("Production Line");
+    primaryStage.setScene(new Scene(root));
     primaryStage.show();
   }
 
