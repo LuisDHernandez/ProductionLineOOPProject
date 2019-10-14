f63af2af54e05d0aa37cb18ebbe3a527983236b9
cobol <cobolongo22@gmail.com>
2019-10-14 19:04:59 -0400

    added OOP project GUI control

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,8 @@ import static sample.ItemType.AUDIO;
 import static sample.ItemType.AUDIO_MOBILE;
 import static sample.ItemType.VISUAL;
 import static sample.ItemType.VISUAL_MOBILE;
+
+import java.util.ArrayList;
 import sample.ItemType.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -70,6 +72,7 @@ public class Controller {
       // STEP 2: Open a connection
       conn = DriverManager.getConnection(DB_URL);
       stmt = conn.createStatement();
+      testMultimedia();
 
       // Clean-up environment
 
@@ -78,6 +81,23 @@ public class Controller {
 
     } catch (SQLException e) { // sql exception needed
       e.printStackTrace();
+    }
+  }
+  public static void testMultimedia() {
+    AudioPlayer newAudioProduct = new AudioPlayer("DP-X1A", "Onkyo",
+        "DSD/FLAC/ALAC/WAV/AIFF/MQA/Ogg-Vorbis/MP3/AAC", "M3U/PLS/WPL");
+    Screen newScreen = new Screen("720x480", 40, 22);
+    MoviePlayer newMovieProduct = new MoviePlayer("DBPOWER MK101", "OracleProduction", newScreen,
+        MonitorType.LCD);
+    ArrayList<MultimediaControl> productList = new ArrayList<MultimediaControl>();
+    productList.add(newAudioProduct);
+    productList.add(newMovieProduct);
+    for (MultimediaControl p : productList) {
+      System.out.println(p);
+      p.play();
+      p.stop();
+      p.next();
+      p.previous();
     }
   }
 }
