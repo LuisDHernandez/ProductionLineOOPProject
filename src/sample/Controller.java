788c62bcf9e47eaa8a7e2e6b84d33823feb7c8ee
cobol <cobolongo22@gmail.com>
2019-11-09 10:07:06 -0500

    Added database functionality; show products in list view and table view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,9 +2,11 @@ package sample;
 
 import static sample.ItemType.*;
 import java.sql.PreparedStatement;
-import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -16,6 +18,8 @@ import java.sql.Statement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 /** @author Luis Hernandez 9/20/2019 */
 public class Controller {
@@ -29,13 +33,27 @@ public class Controller {
 
   @FXML private ComboBox<ItemType> cbxitemType;
 
-  @FXML private TableView<?> tbvExProd;
+  @FXML private TableView<Product> tbvExProd;
 
-  @FXML private ListView<?> chooseProdLSV;
+  @FXML private ListView<Product> chooseProdLSV;
 
   @FXML private ComboBox<Integer> cbxQuantity;
 
   @FXML private Button btnrecordProd;
+
+  @FXML private Button btnaddProduct;
+
+  @FXML private TableColumn<?, ?> colProduct;
+
+  @FXML private TableColumn<?, ?> colManufacturer;
+
+  @FXML private TableColumn<?, ?> colItemType;
+
+  @FXML private TableColumn<?, ?> colID;
+
+  private ObservableList<Product> theProducts = FXCollections.observableArrayList();
+
+  // establish database variables
   private Connection conn;
   private Statement stmt;
 
@@ -59,7 +77,7 @@ public class Controller {
 
       // Clean-up environment
 
-      cbxQuantity.getItems().addAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+      cbxQuantity.getItems().addAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,25,50,100);
       cbxQuantity.setEditable(true);
       cbxQuantity.getSelectionModel().selectFirst();
       cbxitemType.getItems().addAll(AUDIO, VISUAL, AUDIO_MOBILE, VISUAL_MOBILE);
@@ -72,9 +90,10 @@ public class Controller {
       e.printStackTrace();
     }
   }
-  /** @param event when the button is pressed it will add input to Product DB */
+  /** @param event when the button is pressed it will add input to Product DB
+   *  and then show the created products in table/list views for the user to see*/
   @FXML
-  void btnAddProduct(ActionEvent event) {
+  void btnAddProduct(ActionEvent event) throws SQLException {
 
     try {
       String query = "INSERT INTO PRODUCT(name, manufacturer, type) VALUES (?, ?, ?)";
@@ -85,6 +104,48 @@ public class Controller {
 
       ps.executeUpdate();
 
+      System.out.println("item added to db");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    try {
+      String sql = "SELECT * FROM PRODUCT";
+
+      ResultSet rs = stmt.executeQuery(sql);
+
+      while (rs.next()) {
+
+        // these lines correspond to the database table columns
+        int id = rs.getInt(1);
+        String name = rs.getString(2);
+        String type = rs.getString(3);
+        String manufacturer = rs.getString(4);
+        // create object
+        ItemType prodType = null;
+
+        if (type.equals("AU")) {
+          prodType = AUDIO;
+        } else if (type.equals("VI")) {
+          prodType = VISUAL;
+        } else if (type.equals("AM")) {
+          prodType = AUDIO_MOBILE;
+        } else if(type.equals("VM")){
+          prodType = VISUAL_MOBILE;
+        }
+
+        // show in columns
+        colProduct.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colManufacturer.setCellValueFactory(new PropertyValueFactory<>("manufacturer"));
+        colItemType.setCellValueFactory(new PropertyValueFactory<>("type"));
+        colID.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        Product product = new Widget(name, manufacturer, prodType);
+        // save to observable list
+        theProducts.add(product);
+
+        tbvExProd.setItems(theProducts);
+        chooseProdLSV.setItems(theProducts);
+      }
     } catch (SQLException e) {
       e.printStackTrace();
     }
@@ -93,8 +154,7 @@ public class Controller {
    * @param event this action records the product & prints out to the screen that it was successful
    */
   @FXML
-  void btnRecordProduction(ActionEvent event) {
-
-    System.out.println("Product was recorded");
+  void btnRecordProduction(ActionEvent event) throws SQLException {
+    System.out.println("recorded");
   }
 }
--- src/sample/ItemType.java
+++ src/sample/ItemType.java
@@ -3,10 +3,10 @@ package sample;
 /** @author Luis D. Hernandez 9/30/2019 */
 public enum ItemType {
   // AU, VI, AM, VM
-  AUDIO("AU"),
+  AUDIO("AU"), //0
   VISUAL("VI"),
   AUDIO_MOBILE("AM"),
-  VISUAL_MOBILE("VM");
+  VISUAL_MOBILE("VM"); //3
   public final String code;
 
   ItemType(String itemCode) {
